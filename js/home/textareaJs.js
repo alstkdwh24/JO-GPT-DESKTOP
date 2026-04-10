d0d54057394e3778b6d9719ab6d2e1897d61d842
민상 <alstkdwh24@gmail.com>
2026-04-10 20:38:17 +0900

    방키 전달안되는 에러 해결

--- js/home/textareaJs.js
+++ js/home/textareaJs.js
@@ -1,4 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let showChat = localStorage.getItem('showChat');
+    let realBoxFont = document.querySelector('.realBoxFont');
+    const myGemini = document.querySelector(".my-gemini-talk")
+    let realBox = document.querySelector('.realBox');
+    const gptGeminiTalk = document.querySelector('.my-gemini-talk');
+    const tpl = document.getElementById('tpl-gpt-content');
+    const myGeminiTalk = document.querySelector('.my-gemini-talk');
+    const tplLoading = document.getElementById('tpl-loading');
+    const token = localStorage.getItem('ACCESS_TOKEN');
 
     console.log(CONFIG.CUSTOM_SCHEME);
     /*textarea 내용 전송*/
@@ -7,134 +16,125 @@ document.addEventListener('DOMContentLoaded', () => {
         textarea.focus();
     }
     let searchBtn = document.querySelector('.search-real');
+
     searchBtn.onclick = (event) => {
-        sendContents();
-    }
-    textarea.addEventListener("keydown", (event) => {
-        if (event.key === "enter") {
-            event.preventDefault();
+        if (searchBtn.classList.contains('search-real-hide')) {
+            /*첫번째랑 구분할 필요가 있습니다.*/
+            twoSendContents(showChat)
+        } else {
             sendContents();
         }
-    })
-});
-
-
-function sendContents() {
-
-    let textarea = document.querySelector('.fake-input');
-    let realContent = document.querySelector('.realContent');
-    let realBoxFont = document.querySelector('.realBoxFont');
-    const myGemini = document.querySelector(".my-gemini-talk")
-    let gptContents = textarea.value
-    console.log(textarea.value);
-    let token = localStorage.getItem('ACCESS_TOKEN');
-    let realBox = document.querySelector('.realBox');
-
-    if (token != null) {
-        textarea.style.height = 'auto'; // 높이 초기화 추가
-        myGemini.style.gap = '1rem';
-        myGemini.style.height = '80%';
-        myGemini.style.width = '44rem';
-        myGemini.style.display = 'flex';
-        myGemini.style.flexDirection = 'column';
-        myGemini.style.justifyContent = 'flex-start';
-        realBox.style.display = 'flex';
-        realBox.style.alignItems = 'center';
-        realBox.style.justifyContent = 'center';
-        realBox.style.minHeight = '1rem';
-        realBoxFont.style.display = "none";
-        realBoxFont.style.height = '0px';
-        textarea.focus(); // 전송 후 다시 포커스
-        realBoxFont.textContent = "";
-        const myContents = textarea.value;
-        //말풍선 생성
-        MyContents(myContents);
-        textarea.value = "";
-
-        sendContentsAjax(myContents);
-
-
-    } else {
-        alert("로그인 후 이용해주세요");
-
     }
-}
+    textarea.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            if (searchBtn.classList.contains('search-real-hide')) {
+                /*첫번째랑 구분할 필요가 있습니다.*/
+                twoSendContents(showChat)
+            } else {
+                sendContents();
+            }
+        }
+    })
+
+    function sendContents() {
+
+        if (!textarea.value.trim()) {
+            return;
+        }
+        console.log(textarea.value);
+        let token = localStorage.getItem('ACCESS_TOKEN');
+
+
+        if (token != null) {
+            textarea.style.height = 'auto'; // 높이 초기화 추가
+            myGemini.style.gap = '1rem';
+            myGemini.style.height = '80%';
+            myGemini.style.width = '44rem';
+            myGemini.style.display = 'flex';
+            myGemini.style.flexDirection = 'column';
+            myGemini.style.justifyContent = 'flex-start';
+            realBox.style.display = 'flex';
+            realBox.style.alignItems = 'center';
+            realBox.style.justifyContent = 'center';
+            realBox.style.minHeight = '1rem';
+            realBoxFont.style.display = "none";
+            realBoxFont.style.height = '0px';
+            textarea.focus(); // 전송 후 다시 포커스
+            realBoxFont.textContent = "";
+
+
+            const myContents = textarea.value;
+            //말풍선 생성
+            MyContents(myContents);
+            textarea.value = "";
+            sendContentsAjax(myContents).then(response => {
+                return "success";
+            }).catch(error => {
+                return "error"
+            });
+        } else {
+            alert("로그인 후 이용해주세요");
+
+        }
+    }
 
 //내 대화를 말풍선으로 보여주기 위한
-function MyContents(myContents) {
-    const myGeminiTalk = document.querySelector('.my-gemini-talk');
-    const tpl = document.getElementById('tpl-my-content');
-    const clone = tpl.content.cloneNode(true);
-    clone.querySelector('#realMyContent').textContent = myContents; // ✅ XSS 안전    console.log("myContent {}", myContents);
-    myGeminiTalk.appendChild(clone);
-
-}
+    function MyContents(myContents) {
+        const myGeminiTalk = document.querySelector('.my-gemini-talk');
+        const tpl = document.getElementById('tpl-my-content');
+        const clone = tpl.content.cloneNode(true);
+        clone.querySelector('#realMyContent').textContent = myContents; // ✅ XSS 안전    console.log("myContent {}", myContents);
+        myGeminiTalk.appendChild(clone);
+    }
 
 //gpt 대화를 말품선으로 보여주기 위한
-function GPTContents(gptContents) {
-    const gptGeminiTalk = document.querySelector('.my-gemini-talk');
-    const tpl = document.getElementById('tpl-gpt-content');
-    //template 요소의 content를 복제하여 새로운 노드 생성
-    const clone = tpl.content.cloneNode(true)
-
-    //DOMPurity는 xss(크로스 사이트 스크립팅) 공격을 방지하기 위한 HTML 새니타이저 라이브러리입니다. 쉽게 말해 악성 스크립트를 제거해주는 도구 입니다.
-    clone.querySelector('#realGeminiContent').innerHTML = DOMPurify.sanitize(marked.parse(gptContents)) // 마크다운은 innerHTML 필요
-
-    gptGeminiTalk.appendChild(clone);
-}
-
-/*로딩 보여주기*/
-function showLoading() {
-    const myGeminiTalk = document.querySelector('.my-gemini-talk');
-    const tpl = document.getElementById('tpl-loading');
-    const clone = tpl.content.cloneNode(true);
-    myGeminiTalk.appendChild(clone);
-    document.getElementById('start-loading').scrollIntoView({behavior: 'smooth'});
-}
-
-/*로딩 제거*/
-function hideLoading() {
-    const loading = document.getElementById('start-loading');
-    if (loading) {
-        loading.remove();
-    }
-}
-
-/*ajax 연속 코드 가독성 살린 것 내가 쓴 메시지 보내고 gpt응답받는 코드*/
-async function sendContentsAjax(myContents) {
-    const token = localStorage.getItem('ACCESS_TOKEN');
-    if (!token) {
-        alert("로그인 후 이용해주세요");
-        return;
+    function GPTContents(gptContents) {
+        //template 요소의 content를 복제하여 새로운 노드 생성
+        const clone = tpl.content.cloneNode(true)
+        //DOMPurity는 xss(크로스 사이트 스크립팅) 공격을 방지하기 위한 HTML 새니타이저 라이브러리입니다. 쉽게 말해 악성 스크립트를 제거해주는 도구 입니다.
+        clone.querySelector('#realGeminiContent').innerHTML = DOMPurify.sanitize(marked.parse(gptContents)) // 마크다운은 innerHTML 필요
+        gptGeminiTalk.appendChild(clone);
     }
 
-    try {
-        await fetch(CONFIG.API_CONTENTS_URL + '/contents/myContents', {
-            method: "POST",
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({myChatContents: myContents})
-        });
+    /*로딩 보여주기*/
+    function showLoading() {
 
+        const clone = tplLoading.content.cloneNode(true);
+        myGeminiTalk.appendChild(clone);
+        document.getElementById('start-loading').scrollIntoView({behavior: 'smooth'});
+    }
+
+    /*로딩 제거*/
+    function hideLoading() {
+        const loading = document.getElementById('start-loading');
+        if (loading) {
+            loading.remove();
+        }
+    }
+
+    /*ajax 연속 코드 가독성 살린 것 내가 쓴 메시지 보내고 gpt응답받는 코드*/
+    async function sendContentsAjax(myContents) {
+        if (!token) {
+            alert("로그인 후 이용해주세요");
+            return;
+        }
+        try {
+            const response = await fetchSendMessage(token, myContents);
+            if (response) {
+                searchBtn.classList.remove('search-real');
+                searchBtn.classList.add('search-real-hide');
+            }
+        } catch (e) {
+            hideLoading();
+            console.error(e);
+        }
+    }
+
+    async function fetchSendMessage(token, myContents) {
         showLoading();
-
-        const response = await fetch(CONFIG.API_CONTENTS_URL + '/contents/gptContents', {
-            method: "POST",
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({myChatContents: myContents})
-        });
-        const data = await response.json(); // 본문을 JSON으로 파씽을 해야됨
-        // 1. 먼저 필요한 데이터를 꺼냄
-        const gptText = data.candidates[0].content.parts[0].text;
-        console.log(gptText);
-        GPTContents(gptText);
         // 3. 채팅방 생성
-        await fetch(CONFIG.API_CONTENTS_URL + '/contents/chatRoom', {
+        const response2 = await fetch(CONFIG.API_CONTENTS_URL + '/contents/chatRoom', {
             method: "POST",
             headers: {
                 'Authorization': 'Bearer ' + token,
@@ -142,9 +142,91 @@ async function sendContentsAjax(myContents) {
             },
             body: JSON.stringify({myChatContents: myContents})
         })
-        hideLoading();
-    } catch (e) {
-        hideLoading();
-        console.error(e);
+        const showChatKey = await response2.text(); // json() 대신 text()
+        console.log("showChatKey {}", showChatKey);
+                localStorage.setItem('showChat', showChatKey);
+        return gptResponse(myContents).then(response => {
+            hideLoading();
+            return response;
+        }).catch(error => {
+            return "error"
+        });
     }
-}
+
+    /*채팅방 만들어지고 메시지 보낼때*/
+    function twoSendContents(showChat) {
+        console.log("showChatss {}", showChat);
+        let token = localStorage.getItem('showChat');
+
+        if (!textarea.value.trim()) {
+            return;
+        }
+        if (token != null) {
+            textarea.focus(); // 전송 후 다시 포커스
+            realBoxFont.textContent = "";
+
+            const myContents = textarea.value;
+            //말풍선 생성
+            MyContents(myContents);
+            textarea.value = "";
+            twoSendContentsAjax(myContents, showChat).then(response => {
+                return "success";
+            }).catch(error => {
+                return "error"
+            });
+        }
+    }
+
+    async function twoSendContentsAjax(myContents, showChat) {
+        showLoading();
+
+        const response3 = await fetch(CONFIG.API_CONTENTS_URL + '/contents/myContents', {
+            method: "POST",
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                myChatContents: myContents
+                , showChatKey: showChat
+            })
+        });
+        if (!response3.ok) {
+            throw new Error(`HTTP error! status: ${response3.status}`);
+        }
+        const response = gptResponse(myContents).then(response => {
+            hideLoading();
+            return response;
+        }).catch(error => {
+            return "error"
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    }
+
+    async function gptResponse(myContents) {
+        const response = await fetch(CONFIG.API_CONTENTS_URL + '/contents/gptContents', {
+            method: "POST",
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({myChatContents: myContents})
+
+        });
+        const data = await response.json(); // 본문을 JSON으로 파씽을 해야됨
+        // 1. 먼저 필요한 데이터를 꺼냄
+        const gptText = data.candidates[0].content.parts[0].text;
+        if (!gptText) {
+            hideLoading();
+            console.log("GPT 응답 형식 오류:", data);
+            return;
+        }
+        console.log(gptText);
+        GPTContents(gptText);
+        return gptText;
+    }
+});
+
+
